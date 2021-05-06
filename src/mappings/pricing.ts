f18f2b1568552337d5d711a1a1dc1011fea9e359
panther-swap <admin@pantherswap.com>
2021-05-06 22:20:47 +0800

    refactor: Update USDT_WBNB_PAIR address

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -6,7 +6,7 @@ import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD } from './helpers'
 const WBNB_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
 const BUSD_WBNB_PAIR = '0x6af4c4433474b2f8ba385ad62b23299c82846783' // created block 7173211
 const DAI_WBNB_PAIR = '0x9938f3f25d2a4c8d38b829be9fd5f9554c3c900d'  // created block 7183251
-const USDT_WBNB_PAIR = '0x20bcc3b8a0091ddac2d0bc30f68e6cbb97de59cd' // created block 648115
+const USDT_WBNB_PAIR = '0x26782a2669d32be87c892ada10aa630d0834b3c4' // created block 7183335
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
